4b4d6b25448a6c71f0c6d414cbf90ff941f2826e
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2023-12-03 18:04:19 +0300

    Update app.py

--- survepoll/app.py
+++ survepoll/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request, redirect
 from markupsafe import escape
 
 app = Flask(__name__)
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html",methods=["POST"])
+    if request.method == "POST":
+        # Handle the form submission logic here if needed
+        pass
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    #app.run(debug=True)
+    # app.run(debug=True)
     app.run(host='127.0.0.1', port=80)
